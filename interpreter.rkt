e38095cfbb2149594d0244a25eefc670208e3cf1
Zasilus <asg101@case.edu>
2019-02-13 18:55:26 -0500

    Working version of m_value
    Signed-off-by: Zasilus <asg101@case.edu>

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,1 +1,9 @@
 #lang racket
+;It takes a parameter of a variable and the m_state and it returns a state
+(define m_value
+  (lambda (var m_state)
+    (cond
+      [(null? m_state) (error 'undefined_var "Variable is undefined")]
+      [(eq? var (car (car m_state))) (car (car (cdr m_state)))]
+      (else (m_value var (cons (cdar m_state) (list (cdadr m_state))))))))
+    
