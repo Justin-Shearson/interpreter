398750511550dba7092c14a9b7354a01ca7f518d
Zasilus <asg101@case.edu>
2019-02-13 18:56:22 -0500

    added an example to m_value
    Signed-off-by: Zasilus <asg101@case.edu>

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,5 +1,6 @@
 #lang racket
 ;It takes a parameter of a variable and the m_state and it returns a state
+;Example: (m_value 'x '((a b c x) (1 2 3 4))) returns 4
 (define m_value
   (lambda (var m_state)
     (cond
