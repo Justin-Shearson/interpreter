09dd401571d707de6ce4f8bd59584af8370a2784
Zasilus <asg101@case.edu>
2019-02-13 20:10:39 -0500

    Added an m_eval function that evalutates expressions as well as an example in the comments
    Signed-off-by: Zasilus <asg101@case.edu>

--- interpreter.rkt
+++ interpreter.rkt
@@ -7,4 +7,36 @@
       [(null? m_state) (error 'undefined_var "Variable is undefined")]
       [(eq? var (car (car m_state))) (car (car (cdr m_state)))]
       (else (m_value var (cons (cdar m_state) (list (cdadr m_state))))))))
-    
+
+(define rvalue
+  (lambda (exp)
+    (cond
+      [(null? exp) (error 'undefined "undefined expression")]
+      [(number? exp) exp]
+      [(eq? (car exp) '+) (+ (rvalue (car (cdr exp))) (rvalue (car (cdr (cdr exp)))))]
+      [(eq? (car exp) '-) (- (rvalue (car (cdr exp))) (rvalue (car (cdr (cdr exp)))))]
+      [(eq? (car exp) '/) (quotient (rvalue (car (cdr exp))) (rvalue (car (cdr (cdr exp)))))]
+      [(eq? (car exp) '*) (* (rvalue (car (cdr exp))) (rvalue (car (cdr (cdr exp)))))]
+      [(eq? (car exp) '%) (remainder (rvalue (car (cdr exp))) (rvalue (car (cdr (cdr exp)))))])))
+
+;m_eval takes an expression and returns the value of the expression
+;Example: (m_eval '(+ x y) '((x y) (5 10))) returns 15 because x and y are stored in the state
+
+(define m_eval
+  (lambda (exp m_state)
+    (cond
+      [(null? exp) (error 'null_expression "Expression is null")]
+      [(number? exp) exp]
+      [(symbol? exp) (m_value exp m_state)]
+      [(eq? (car exp) '+) (+ (m_eval (operand1 exp) m_state) (m_eval (operand2 exp) m_state))]
+      [(eq? (car exp) '-) (- (m_eval (operand1 exp) m_state) (m_eval (operand2 exp) m_state))]
+      [(eq? (car exp) '/) (quotient (m_eval (operand1 exp) m_state) (m_eval (operand2 exp) m_state))]
+      [(eq? (car exp) '*) (* (m_eval (operand1 exp) m_state) (m_eval (operand2 exp) m_state))]
+      [(eq? (car exp) '%) (remainder (m_eval (operand1 exp) m_state) (m_eval (operand2 exp) m_state))])))
+
+(define operator car)
+(define operand1 cadr)
+(define operand2 caddr)
+
+
+
