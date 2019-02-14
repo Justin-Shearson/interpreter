5e46ca96abc93010b0606bbd7ff5c63d2a239c38
Justin <justin.shearson@case.edu>
2019-02-13 20:38:38 -0500

    Corrected error in m_bool

--- interpreter.rkt
+++ interpreter.rkt
@@ -7,7 +7,7 @@
     (cond
       [(null? condition) (error 'invalid "expression is empty")]
       [(symbol? condition) (m_value condition m_state)]
-      [(or (number? condition) (boolean? condition) condition)]
+      [(or (number? condition) (boolean? condition) condition]
       [(eq? (get_op condition) '&&) (and (m_bool (left_operand condition) m_state return) (m_bool (right_operand condition) m_state return))]
       [(eq? (get_op condition) '||) (or (m_bool (left_operand condition) m_state return) (m_bool (right_operand condition) m_state return))]
       [(eq? (get_op condition) '>=) (>= (m_bool (left_operand condition) m_state return) (m_bool (right_operand condition) m_state return))]
