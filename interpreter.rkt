a002e95718ec08abbf13e4cf0768d156f1227554
Zasilus <asg101@case.edu>
2019-03-07 22:05:44 -0500

    Created support and meaningful abstraction for loop_state and loop_breaking changed some variable names to more properly represent their values
    Signed-off-by: Zasilus <asg101@case.edu>

--- interpreter.rkt
+++ interpreter.rkt
@@ -164,12 +164,12 @@
 
 (define loop_state
   (lambda (m_state)
-    (s_declare '8=========================D m_state)))
+    (s_declare 'loop--state m_state)))
 
 (define removeBreakLayer
   (lambda (m_state)
     (cond
-      [(s_member_layer '8=========================D (cadr m_state)) (removeLayer m_state)]
+      [(s_member_layer 'loop--state (cadr m_state)) (removeLayer m_state)]
       [else (removeBreakLayer (cadr m_state))])))
 ;;Helper function that returns true if there exists a sublayer false if there isn't a layer 
 (define s_layer
